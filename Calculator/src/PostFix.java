53cfefa28f68134ec320d8ef327eaf3fa9e4e938
allanbud <66875988+allanbud@users.noreply.github.com>
2020-07-05 17:47:18 +0300

    redone to Factory Class

--- Calculator/src/PostFix.java
+++ Calculator/src/PostFix.java
@@ -2,12 +2,12 @@ import java.util.*;
 
 public class PostFix {
 
-    private static boolean pass, zero, prefix;
+    private static boolean pass, zero, prefix, wrongOperator;
 
     private static Scanner in = new Scanner(System.in);
 
 
-/***********************************************************************************/
+/*****************checks if the symbol entered by user in a number (double)*********************/
     private static boolean isNumber(String string)  {
         try{
             Double.parseDouble(string);
@@ -16,31 +16,7 @@ public class PostFix {
             return false;
         }
     }
-/************************************************************************************/
 
-    private static String[] reverseStr(String[] string){
-        int i = 0;
-        int n = 0;
-        int len = string.length - 1;
-        String[] dest = new String[len+1];
-
-        for (int t = 0; t == len; t++){
-            dest[t] = string[t];
-        }
-
-        while (!isNumber(string[i])) {
-            dest[len] = string[i];
-            len--;
-            i++;
-        }
-
-        for (int j = i; j <= len + i; j++){
-            dest[n] = string[j];
-            n++;
-        }
-
-        return dest;
-    }
 
 /******** define an Operation interface which has a single apply method ********/
     public interface Operation {
@@ -107,26 +83,13 @@ public static double calculateUsingFactory(double a, double b, String operator) 
 
     public static void main(String[] args){
 
-/*
-        System.out.println("post or pref?");
-        String menu = in.nextLine();
-        if (menu.equals("post")) {
-            prefix = true;
-        }
-
- */
-
-        System.out.println("please enter what you need to enter");
+        System.out.println("Please enter Postfix calculator string,");
+        System.out.println("use spaces to separate numbers and operators:");
         String str = in.nextLine();
 
 
         String[] strings = str.split(" ");
-/*
-        if (!prefix) {
-            strings = reverseStr(strings);
-        }
 
- */
 
         Stack<Double> stack = new Stack<Double>();
 
@@ -140,62 +103,20 @@ public static double calculateUsingFactory(double a, double b, String operator) 
 
                 if (stack.size() >= 2 && strings.length % 2 != 0) {
                     pass = true;
-                    double tmp1 = stack.pop();
-                    double tmp2 = stack.pop();
-                    stack.push(calculateUsingFactory(tmp2, tmp1, strings[i]));
-
-/*
+                    double b = stack.pop();
+                    double a = stack.pop();
+                    stack.push(calculateUsingFactory(a, b, strings[i]));
 
 
-                    switch (strings[i]) {
 
-                        case "+":
-
-                            stack.push(tmp1 + tmp2);
-
-                            break;
-
-                        case "-":
-
-                            stack.push(tmp2 - tmp1);
-
-                            break;
-
-                        case "*":
-
-                            stack.push(tmp1 * tmp2);
-
-                            break;
-
-
-                        case "/":
-                            try {
-                                double div = (tmp2 / tmp1);
-                                stack.push(div);
-                                if (div == Double.POSITIVE_INFINITY ||
-                                        div == Double.NEGATIVE_INFINITY) {
-                                    throw new ArithmeticException();
-                                }
-                            } catch (ArithmeticException ae) {
-                                zero = true;
-                                System.out.print(Color.RED_BOLD_BRIGHT);
-                                System.out.println("ArithmeticException occured, div by zero!");
-                                System.out.print(Color.RESET);
-                            }
-                            break;
-
-                        default:
-// if any symbol that is not an operator will result an error
-// clean stack is an error automatically
-// i = string.length will break the loop
-                        {
+                if (wrongOperator){
                             stack.clear();
                             i = strings.length;
-                        }
+
+                }
 
 
-                    }
-*/
+
                 }
             }
         }
