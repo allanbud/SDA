39208620cedd78aebb8f99e155a3b9bd1d265c74
allanbud <66875988+allanbud@users.noreply.github.com>
2020-09-15 14:09:07 +0300

    Added go to login after register

--- MyFirstGUI/src/sample/Configs.java
+++ MyFirstGUI/src/sample/Configs.java
@@ -1,9 +1,11 @@
 package sample;
 
 public class Configs {
-    protected String dbHost = "localhost";
+
+
+    protected String dbHost = "remotemysql.com";
     protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "SurokSurok";
-    protected String dbName = "myfirstgui";
+    protected String dbUser = "KDWVTJ0fe7";
+    protected String dbPass = "DDvie8IzIA";
+    protected String dbName = "KDWVTJ0fe7";
 }
